9fc70e126026229672e8610c2e849552ecd67a5f
Minahil Akhtar <89981760+minahilakhtar@users.noreply.github.com>
2022-12-21 06:42:20 -0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="">Home</Link>
+              <Link className="nav-link active" aria-current="page" to="/text-utils">Home</Link>
             </li>
           </ul>
           {/* <form className="d-flex" role="search">
